0af4cd80bac07fce85abcf63fa82022f27a3f62c
Daniel Colson <danieljamescolson@gmail.com>
2016-02-12 16:41:14 -0500

    Add router

---
+++ controller/lib/router.rb
@@ -0,0 +1,59 @@
+class Route
+  attr_reader :pattern, :http_method, :controller_class, :action_name
+
+  def initialize(pattern, http_method, controller_class, action_name)
+    @pattern, @http_method, @controller_class, @action_name =
+      pattern, http_method, controller_class, action_name
+  end
+
+  def matches?(req)
+    pattern =~ req.fullpath &&
+      http_method == req.request_method.downcase.to_sym
+  end
+
+  def run(req, res)
+    match_data = pattern.match(req.fullpath)
+    route_params = Hash[match_data.names.zip(match_data.captures)]
+
+    controller = controller_class.new(req, res, route_params)
+    controller.invoke_action(action_name)
+  end
+end
+
+class Router
+  attr_reader :routes
+
+  def initialize
+    @routes = []
+  end
+
+  def add_route(pattern, http_method, controller_class, action_name)
+    @routes << Route.new(pattern, http_method, controller_class, action_name)
+  end
+
+  def draw(&proc)
+    instance_eval(&proc)
+  end
+
+  [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
+  end
+
+  def match(req)
+    @routes.find { |route| route.matches?(req) }
+  end
+
+  def run(req, res)
+    route = match(req)
+
+    if route.nil?
+      res.status = 404
+
+      res.write("Oops! The requested URL #{req.fullpath} was not not found!")
+    else
+      route.run(req, res)
+    end
+  end
+end
