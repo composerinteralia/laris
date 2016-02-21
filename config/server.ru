1b5deb3046e5c8e277fcf9056ca0431162ba3757
Daniel Colson <danieljamescolson@gmail.com>
2016-02-21 13:42:00 -0500

    Add basic migrations table

--- config/server.ru
+++ config/server.ru
@@ -6,7 +6,7 @@ require_relative 'lib/asset_server'
 require_relative 'lib/exception_viewer'
 require_relative '../db/lib/db_connection'
 
-DBConnection.migrate
+DBConnection.open
 
 asset_app = Proc.new do |env|
   req = Rack::Request.new(env)
--- db/lib/db_connection.rb
+++ db/lib/db_connection.rb
@@ -20,14 +20,34 @@ class DBConnection
   end
 
   def self.migrate
-    APP_SQL_FILES.each do |file|
-      instance.exec(File.read(file))
-    end
-  end
+    # res = instance.exec(<<-SQL)
+    #   SELECT EXISTS (
+    #     SELECT
+    #       1
+    #     FROM
+    #       pg_catalog.pg_class c
+    #     WHERE
+    #       c.relname = 'migrations' AND
+    #       c.relkind = 'r')
+    # SQL
+    #
+    # res[0]['exists'] == "t"
 
-  def self.instance
-    open if @conn.nil?
-    @conn
+    ensure_migrations_table
+
+    APP_SQL_FILES.each do |file|
+      filename = file.match(/([\w|-]*)\.sql$/)[1]
+
+      unless migrated_files.include?(filename)
+        instance.exec(File.read(file))
+        instance.exec(<<-SQL)
+          INSERT INTO
+            migrations (filename)
+          VALUES
+            ('#{filename}')
+          SQL
+      end
+    end
   end
 
   def self.execute(query, params=[])
@@ -37,20 +57,60 @@ class DBConnection
   end
 
   def self.columns(table_name)
-    instance.exec('SELECT * FROM information_schema.columns')
-      .select { |x| x['table_name'] == table_name }
-      .map { |x| x['column_name'].to_sym }
+    # cols = instance.exec(<<-SQL)
+    #   SELECT
+    #     column_name
+    #   FROM
+    #     information_schema.columns
+    #   WHERE
+    #     table_name = '#{table_name}'
+    # SQL
+    #
+    # cols.map { |col| col['column_name'].to_sym }
+
+    cols = instance.exec(<<-SQL)
+      SELECT
+        attname
+      FROM
+        pg_attribute
+      WHERE
+        attrelid = '#{table_name}'::regclass AND
+        attnum > 0 AND
+        NOT attisdropped
+    SQL
+
+    cols.map { |col| col['attname'].to_sym }
   end
 
   private
 
-  def self.print_query(query, interpolation_args)
-    puts '--------------------'
-    puts query
-    unless interpolation_args.empty?
-      puts "interpolate: #{interpolation_args.inspect}"
+  def self.instance
+    open if @conn.nil?
+    @conn
+  end
+
+  def self.ensure_migrations_table
+    res = instance.exec(<<-SQL)
+      SELECT to_regclass('migrations') AS exists
+    SQL
+
+    unless res[0]['exists']
+      instance.exec(<<-SQL)
+        CREATE TABLE migrations (
+          id SERIAL PRIMARY KEY,
+          filename VARCHAR(255) NOT NULL
+        )
+      SQL
     end
-    puts '--------------------'
+  end
+
+  def self.migrated_files
+    Set.new instance.exec(<<-SQL).values.flatten
+      SELECT
+        filename
+      FROM
+        migrations
+    SQL
   end
 
   def self.number_placeholders(query_string)
@@ -63,5 +123,14 @@ class DBConnection
         char
       end
     end.join("")
+  end
+
+  def self.print_query(query, interpolation_args)
+    puts '--------------------'
+    puts query
+    unless interpolation_args.empty?
+      puts "interpolate: #{interpolation_args.inspect}"
+    end
+    puts '--------------------'
   end
 end
